aa52ee17966e63e8a9e2b8cabc1d263e71fea8f1
confluencendd <confluence.ndd.analytics@gmail.com>
2024-10-01 11:44:04 -0300

    reducing if

--- src/js/utils/breadcrumbs.js
+++ src/js/utils/breadcrumbs.js
@@ -2,11 +2,7 @@ export function formatBreadcrumbs() {
 
     const breadcrumbHome = document.querySelector('ol.breadcrumbs > li:nth-child(1) > a')
 
-    if (!breadcrumbHome) {
-        console.warn(`WARNING: Breadcrumb not found`);
-        return;
-    }
+    if (!breadcrumbHome) return;
 
     breadcrumbHome.textContent = ""
-
 }
