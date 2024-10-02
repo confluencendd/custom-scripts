919431eb0943fa81238943938ce49649bd7b9145
confluencendd <confluence.ndd.analytics@gmail.com>
2024-10-02 14:02:06 -0300

    Created individual hc scripts

---
+++ src/nddorbix/app-hc-nddorbix.js
@@ -0,0 +1,36 @@
+import * as styles from '../css/theme/_nddorbix.css';
+
+import { getCurrentLanguage } from '../js/utils/getLanguage'
+import { getCurrentProduct } from '../js/utils/getProduct';
+import { runFunctionByRoute } from '../js/utils/route'
+import { redirectLinks } from '../js/utils/redirect'
+
+import { translateTiles } from '../components/translation/tiles';
+import { translateCategories } from '../components/translation/categories';
+import { translateHeaderLinks } from '../components/translation/headerLinks';
+import { translatePlaceholderSearchBar } from '../components/translation/placeholder';
+
+import { formatBreadcrumbs } from '../js/utils/breadcrumbs';
+import { createTimeToReadComponent } from '../components/header-bar/header-bar';
+
+const languagePicked = getCurrentLanguage();
+const product = getCurrentProduct();
+
+
+runFunctionByRoute(() => translateTiles(product, languagePicked), '/');
+
+runFunctionByRoute(() => translateCategories(product, languagePicked), '/');
+
+runFunctionByRoute(() => translateHeaderLinks(product, languagePicked));
+
+runFunctionByRoute(() => redirectLinks(product, languagePicked), '/');
+
+runFunctionByRoute(() => translatePlaceholderSearchBar(languagePicked));
+
+runFunctionByRoute(() => formatBreadcrumbs());
+
+runFunctionByRoute(() => createTimeToReadComponent(languagePicked))
+
+
+
+
