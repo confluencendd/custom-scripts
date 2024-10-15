144cf598beb05dcfb2c7d8cfa20a87e3fad94f64
Catarine Maltauro da Silva <catarinemaltauro@uniplaclages.edu.br>
2024-10-15 16:06:18 -0300

    Alterações de tradução

--- src/nddorbix/app-hc-nddorbix.js
+++ src/nddorbix/app-hc-nddorbix.js
@@ -3,7 +3,6 @@ import * as styles from '../css/theme/_nddorbix.css';
 import { getCurrentLanguage } from '../js/utils/getLanguage'
 import { getCurrentProduct } from '../js/utils/getProduct';
 import { runFunctionByRoute } from '../js/utils/route'
-import { redirectLinks } from '../js/utils/redirect'
 
 import { translateTiles } from '../components/translation/tiles';
 import { translateCategories } from '../components/translation/categories';
@@ -16,14 +15,11 @@ import { createTimeToReadComponent } from '../components/header-bar/time-to-read
 const languagePicked = getCurrentLanguage();
 const product = getCurrentProduct();
 
-
 runFunctionByRoute(() => translateTiles(product, languagePicked), '/');
 
 runFunctionByRoute(() => translateCategories(product, languagePicked), '/');
 
 runFunctionByRoute(() => translateHeaderLinks(product, languagePicked));
-
-runFunctionByRoute(() => redirectLinks(product, languagePicked), '/');
 
 runFunctionByRoute(() => translatePlaceholderSearchBar(languagePicked));
 
